428c6ce3a11b4ce4b837d68781a97396ca72d378
MATEO BAYA <mateo.baya@gmail.com>
2021-04-08 12:38:52 -0300

    TP1 completado

--- Calculadora/src/menu.h
+++ Calculadora/src/menu.h
@@ -2,8 +2,13 @@
 #define MENU_H_
 
 int menu(int valor1, int valor2, int* valResultado, float* valResultadoFloat, int opcionUsuario);
-/*
- *
+/* @brief Menu que revisa el valor elegido por el usuario, que significara en un switch de funciones matematicas
+ * @param valor1 Toma el primer valor dado por el usuario en main
+ * @param valor2 Toma el segundo valor dado por el usuario en main
+ * @param valResultado Es un puntero que pasara el resultado de la funcion una variable en main
+ * @param valResultadoFloat Es un puntero que pasara el resultado de la funcion una variable en main, se usa para las varibles float
+ * @param opcionUsuario Revisa el numero ingresado por el usuario en main para pasarlo por un switch
+ * @return Se utiliza para saber si la funcion ocurrio correctamente, o si hubo un problema
  */
 
 #endif /* MENU_H_ */
